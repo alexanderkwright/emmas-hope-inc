aea59b5168f5feeef4424e2e936a577235fa14de
akwright <alexander.k.wright@gmail.com>
2019-09-02 21:03:26 -0400

    Cleaned up gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,11 @@
-const gulp = require('gulp');
+const {series, parallel, src, dest, watch} = require('gulp');
 const plugins = require('gulp-load-plugins');
 const critical = require('critical').stream;
 const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync');
 const browserify = require('browserify');
 const buffer = require('vinyl-buffer');
-const cache = require('gulp-cache');
-const imagemin = require('gulp-imagemin');
-const notify = require('gulp-notify');
 const source = require('vinyl-source-stream');
-const fs = require('fs');
 
 const paths = {
   js: {
@@ -19,10 +15,6 @@ const paths = {
   css: {
     src: './src/scss/',
     dest: './public/css/'
-  },
-  images: {
-    src: './src/img/**/*',
-    dest: './public/img/'
   }
 };
 
@@ -30,8 +22,8 @@ const paths = {
 /* ----------------- */
 /* Development
 /* ----------------- */
-
-gulp.task('development', ['scripts', 'templates', 'styles'], () => {
+//gulp.series('scripts', 'templates', 'styles)
+function development() {
   browserSync({
     server: {
       baseDir: './public/'
@@ -47,17 +39,17 @@ gulp.task('development', ['scripts', 'templates', 'styles'], () => {
     }
   });
 
-  gulp.watch(paths.css.src + '**/*.scss', ['styles']);
-  gulp.watch(paths.js.src + '**/*.js', ['scripts']);
-  gulp.watch('./pages/**/*.html', ['templates']);
-});
+  watch(paths.css.src + '**/*.scss', styles);
+  watch(paths.js.src + '**/*.js', scripts);
+  watch('./pages/**/*.html', templates);
+};
 
 
 /* ----------------- */
 /* Scripts
 /* ----------------- */
 
-gulp.task('scripts', () => {
+function scripts() {
   return browserify({
     'entries': [paths.js.src + 'index.js'],
     'debug': true
@@ -77,34 +69,34 @@ gulp.task('scripts', () => {
   .pipe(buffer())
   .pipe(plugins().sourcemaps.init({'loadMaps': true}))
   .pipe(plugins().sourcemaps.write('.'))
-  .pipe(gulp.dest(paths.js.dest))
+  .pipe(dest(paths.js.dest))
   .pipe(browserSync.stream());
-});
+};
 
 
 
 /* ----------------- */
 /* Templates
 /* ----------------- */
-gulp.task('templates', () => {
-  return gulp.src('./pages/**/*.+(html|nunjucks)')
+function templates(){
+  return src('./pages/**/*.+(html|nunjucks)')
     .pipe(plugins().data(function() {
       return require('./src/data.json')
     }))
     .pipe(plugins().nunjucksRender({
       path: ['./templates/']
     }))
-    .pipe(gulp.dest('public'))
+    .pipe(dest('public'))
     .pipe(browserSync.stream());
-});
+};
 
 
 /* ----------------- */
 /* Styles
 /* ----------------- */
 
-gulp.task('styles', () => {
-  return gulp.src(paths.css.src + '**/*.scss')
+function styles() {
+  return src(paths.css.src + '**/*.scss')
     .pipe(plugins().sassGlob())
     .pipe(plugins().sourcemaps.init())
     .pipe(plugins().postcss([
@@ -112,30 +104,30 @@ gulp.task('styles', () => {
     ], { syntax: require('postcss-scss') }))
     .pipe(plugins().sass().on('error', plugins().sass.logError))
     .pipe(plugins().sourcemaps.write())
-    .pipe(gulp.dest(paths.css.dest))
+    .pipe(dest(paths.css.dest))
     .pipe(browserSync.stream());
-});
+};
 
 
 /* ----------------- */
 /* Cssmin
 /* ----------------- */
 
-gulp.task('cssmin', () => {
-  return gulp.src(paths.css.src + '**/*.scss')
+function cssmin() {
+  return src(paths.css.src + '**/*.scss')
     .pipe(plugins().sassGlob())
     .pipe(plugins().sass({
       'outputStyle': 'compressed'
     }).on('error', plugins().sass.logError))
-    .pipe(gulp.dest(paths.css.dest));
-});
+    .pipe(dest(paths.css.dest));
+};
 
 
 /* ----------------- */
 /* Jsmin
 /* ----------------- */
 
-gulp.task('jsmin', () => {
+function jsmin() {
   var envs = plugins().env.set({
     'NODE_ENV': 'production'
   });
@@ -148,25 +140,25 @@ gulp.task('jsmin', () => {
   .pipe(source('index.min.js'))
   .pipe(envs)
   .pipe(buffer())
-  .pipe(plugins().uglify().on('error', plugins().util.log))
+  .pipe(plugins().uglify())
   .pipe(envs.reset)
-  .pipe(gulp.dest(paths.js.dest));
-});
+  .pipe(dest(paths.js.dest));
+};
 
 
 
 // Generate & Inline Critical-path CSS
-gulp.task('critical', function () {
-  return gulp.src('public/**/*.html')
+function buildCritical() {
+  return src('public/**/*.html')
     .pipe(critical({base: 'public/', inline: true, minify: true, css: ['public/css/emmashopeinc.css']}))
     .on('error', function(err) { gutil.log(gutil.colors.red(err.message)); })
-    .pipe(gulp.dest('public'));
-});
+    .pipe(dest('public'));
+};
 
 /* ----------------- */
 /* Taks
 /* ----------------- */
 
-gulp.task('default', ['development']);
-gulp.task('deploy', ['cssmin', 'jsmin']);
-gulp.task('crit', ['critical']);
+exports.default = series(parallel(scripts, templates, styles), development);
+exports.deploy = parallel(cssmin, jsmin);
+exports.crit = series(buildCritical);
